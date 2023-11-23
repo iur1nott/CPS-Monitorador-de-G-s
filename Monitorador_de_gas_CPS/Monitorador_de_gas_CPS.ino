1ab05766436c096d80347f7ac64b4dddb1bb19ec
Felipe Forte <felipeforte@protonmail.com>
2023-11-23 08:48:34 -0300

    limpeza

--- Monitorador_de_gas_CPS/Monitorador_de_gas_CPS.ino
+++ Monitorador_de_gas_CPS/Monitorador_de_gas_CPS.ino
@@ -36,42 +36,12 @@ void setup() {
   lcd.backlight();
 
   lcd.createChar(0, smile);
-
-  if(modo == 0) {
-    lcd.setCursor(5, 0);
-    lcd.print("C");
-    //lcd.cursor();
-    //lcd.blink();
-  }
-
-  if(modo == 2){
-    lcd.home();
-    lcd.write(224);
-    lcd.write(B11111100);
-    lcd.write(126);
-    lcd.write(B00100110);
-  }
-
-  if(modo == 4) {
-    lcd.home();
-    lcd.print("Ciber Fisico");
-  }
-
-  if(modo == 5) {
-    lcd.rightToLeft();
-  }
-
-  if(modo == 6) {
-    lcd.home();
-    lcd.write(0);
-  }
 }
 
 void loop() {
   
-  if(modo == 1) {
 
-    // Lê o pino analógico do sensor
+  // Lê o pino analógico do sensor
   int leitura_analogica = analogRead(pinoAnalogico);
   int flag = true;
 
@@ -82,36 +52,33 @@ void loop() {
   Serial.print("leitura do sensor: ");
   Serial.println(leitura_analogica);
  
-  // Verifica se existe gás tóxico presente no ambiente
   
  
-// Repete a leitura do sensor a cada 1 segundo
+  // Repete a leitura do sensor a cada 1 segundo
 
-    lcd.clear();
-    lcd.setCursor(0,0); // Coluna, Linha
-    lcd.print("Leitura:");
-    lcd.print(leitura_analogica);
-    
-    lcd.setCursor(0,3);
-    lcd.print("Tempo decorrido:");
-    lcd.print(segundos++);
-    delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0); // Coluna, Linha
+  lcd.print("Leitura:");
+  lcd.print(leitura_analogica);
+
+  lcd.setCursor(0,3);
+  lcd.print("Tempo decorrido:");
+  lcd.print(segundos++);
+  delay(1000);
 
  
-    if(leitura_analogica<100){
-      digitalWrite(ledVerde,HIGH);
-      digitalWrite(ledAmarelo,LOW);
-      digitalWrite(ledVermelho,LOW);
-    }else if (leitura_analogica>100 && leitura_analogica<230){
-     digitalWrite(ledVerde,LOW);
-     digitalWrite(ledAmarelo,HIGH);
-     digitalWrite(ledVermelho,LOW);
-    }else{
-      digitalWrite(ledVerde,LOW);
-      digitalWrite(ledAmarelo,LOW);
-      digitalWrite(ledVermelho,HIGH);
+  if(leitura_analogica<100){
+    digitalWrite(ledVerde,HIGH);
+    digitalWrite(ledAmarelo,LOW);
+    digitalWrite(ledVermelho,LOW);
+  } else if (leitura_analogica>100 && leitura_analogica<230){
+   digitalWrite(ledVerde,LOW);
+   digitalWrite(ledAmarelo,HIGH);
+   digitalWrite(ledVermelho,LOW);
+  } else{
+    digitalWrite(ledVerde,LOW);
+    digitalWrite(ledAmarelo,LOW);
+    digitalWrite(ledVermelho,HIGH);
     
-  }
-
   }
 }
