81c5719efa2803af92233285b7ca4a4a81f62c12
Felipe Forte <felipeforte@protonmail.com>
2023-11-23 16:34:27 -0300

    adicionada parte do MQTT

--- Monitorador_de_gas_CPS/Monitorador_de_gas_CPS.ino
+++ Monitorador_de_gas_CPS/Monitorador_de_gas_CPS.ino
@@ -1,4 +1,16 @@
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "nome_da_rede"; // Nome da rede WiFi
+const char* password = "senha_secreta"; // Senha da rede WiFi
+
+/* CONFIGURAÇÕES DO MQTT*/
+const char* mqttServer = "broker.hivemq.com"; // Endereço do Broker MQTT
+const int mqttPort = 1883; // Porta TCP do Broker MQTT
+const char* mqttUser = ""; // Usuário MQTT
+const char* mqttPassword = ""; // Senha MQTT
+
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
@@ -7,24 +19,55 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 #define ledAmarelo 14
 #define ledVerde 27
 
-
-int modo = 1;
-
 int segundos = 0;
 
-byte smile[8] = {
-  B00000,
-  B10001,
-  B00000,
-  B00000,
-  B10001,
-  B01110,
-  B00000,
-  B00000
-};
+WiFiClient espClient; // Cliente de Rede WiFi
+PubSubClient clientMqtt(espClient); // Cria uma instancia de um cliente MQTT
 
 void setup() {
   Serial.begin(9600);
+  WiFi.begin(ssid, password); // Configura o WiFi
+
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print("Tentando se conectar na rede: ");
+        Serial.println(ssid);
+  }
+
+  Serial.println("Conectado na Rede WiFi.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  clientMqtt.setServer(mqttServer, mqttPort);
+  clientMqtt.setCallback(callback);
+
+  /* Verifica se o cliente está conectado ao Broker */
+  while (!clientMqtt.connected()) {     
+      Serial.println("Conectando ao Broker MQTT...");
+      
+      String clientId = "ESP32Client_" + String(random(0xffff), HEX);
+      Serial.println("clientId = " + clientId);
+      
+      /* Conecta o cliente ao Broker MQTT.
+         Configurações de credenciais e Last Will podem ser configuradas aqui*/
+      if (clientMqtt.connect(clientId.c_str(), mqttUser, mqttPassword )) {
+        Serial.println("O cliente " + clientId + " foi conectado com sucesso");
+      } else {
+        // Estado do cliente MQTT. Quando a conexão falhar pode ser usado para obter informações sobre a falha
+        int clientState = clientMqtt.state();
+        
+        Serial.print("Falha ao se conectar. ");
+        Serial.println("Estado do cliente: " + (String) clientState);
+        
+        delay(2000);
+      }
+  }
+
+  Serial.print("Tentando enviar a mensagem");
+  
+  clientMqtt.publish("1yG4Lcv0/esp32", "Hello from ESP32");
+  clientMqtt.subscribe("1yG4Lcv0/led");
+}
 
   // Define os pinos do sensor como entrada
   pinMode(pinoAnalogico, INPUT);
@@ -34,15 +77,43 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
-
-  lcd.createChar(0, smile);
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+    Serial.print("Uma mensagem chegou no tópico: ");
+    Serial.println(topic);
+
+    Serial.print("Payload: ");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+    }
+
+    if (payload[0] == '0'){
+        Serial.println("\nDesligando luz");
+        digitalWrite(led, LOW);
+    }
+
+    if (payload[0] == '1'){
+        Serial.println("\nLigando luz");
+        digitalWrite(led, HIGH);
+    }
+
+    Serial.println();
+    Serial.println(" -----------------------");
+
+}
+
 
 void loop() {
   
-
+  /* loop: função que deve ser chamada regularmente para permitir que o cliente
+  processe as mensagens recebidas e mantenha sua conexão com o Broker.*/
+  clientMqtt.loop();
+  
   // Lê o pino analógico do sensor
-  int leitura_analogica = analogRead(pinoAnalogico);
+  int leitura = analogRead(pinoAnalogico);
+  int res = map(leitura, 0, 4095, 0, 100);
   int flag = true;
 
  
@@ -50,7 +121,7 @@ void loop() {
   
   // Apresenta a leitura analógica no monitor serial
   Serial.print("leitura do sensor: ");
-  Serial.println(leitura_analogica);
+  Serial.println(leitura);
  
   
  
@@ -59,7 +130,7 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0,0); // Coluna, Linha
   lcd.print("Leitura:");
-  lcd.print(leitura_analogica);
+  lcd.print(leitura);
 
   lcd.setCursor(0,3);
   lcd.print("Tempo decorrido:");
@@ -67,11 +138,11 @@ void loop() {
   delay(1000);
 
  
-  if(leitura_analogica<100){
+  if(leitura<100){
     digitalWrite(ledVerde,HIGH);
     digitalWrite(ledAmarelo,LOW);
     digitalWrite(ledVermelho,LOW);
-  } else if (leitura_analogica>100 && leitura_analogica<230){
+  } else if (leitura>100 && leitura<230){
    digitalWrite(ledVerde,LOW);
    digitalWrite(ledAmarelo,HIGH);
    digitalWrite(ledVermelho,LOW);
@@ -81,4 +152,6 @@ void loop() {
     digitalWrite(ledVermelho,HIGH);
     
   }
+  String payload = (String) res;
+  clientMqtt.publish("1yG4Lcv0/sensor", payload.c_str());
 }
